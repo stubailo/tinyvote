2c8fd6672dbd738d0260841b505c57133cd467c0
Sashko Stubailo <sashko@mit.edu>
2014-02-26 14:26:20 -0500

    Added comments

--- packages/voting/instantRunoff.js
+++ packages/voting/instantRunoff.js
@@ -1,14 +1,17 @@
 var tallyVotes = function (remainingCandidates, votes) {
   var points = {};
 
+  // initialize all points to 0
   _.each(remainingCandidates, function (candidate) {
     points[candidate] = 0;
   });
 
-  // give points to remaining candidates
-  // 1 point for first place
-  // x/(10 * votes.length) for each subsequent place for tiebreaker
-
+  // give points to remaining candidates, ignoring votes for candidates that
+  // are no longer in the running:
+  //    1 point for first place
+  //    x/(10 * votes.length) for each subsequent place for tiebreaker
+  // these weights are designed so that a first place preference is always
+  // worth more than all second places, etc
   _.each(votes, function (vote) {
     var weight = 1;
 
@@ -24,13 +27,20 @@ var tallyVotes = function (remainingCandidates, votes) {
 };
 
 Voting = {
+  // takes a list of strings that are the candidates,
+  // and a list of objects that contain an array called candidates
+  // that encode each voter's preferences. See the tests for examples.
   instantRunoff: function (candidates, votes) {
+    // as soon as a candidate gains a majority of first preferences, they win
     var majority = votes.length / 2;
 
     while (candidates.length > 1) {
+      // assign every candidate points based on the candidates that are still
+      // in the running
       var points = tallyVotes(candidates, votes);
-      var pairs = _.pairs(points);
 
+      // reformat to sorted array of pairs of [candidate, points]
+      var pairs = _.pairs(points);
       var sorted = _.sortBy(pairs, function (pair) {
         return -pair[1];
       });
@@ -40,13 +50,16 @@ Voting = {
         return sorted[0][0];
       }
 
+      // if we have no winner and there are 2 candidates, it's a tie
       if (candidates.length === 2) {
         return null;
       }
 
-      // XXX identify ties...
+      // if there is no winner and more than two candidates left, remove the
+      // candidate with the least points and try again
       sorted.pop();
 
+      // reformat back to a dictionary of candidates -> points
       candidates = _.map(sorted, function (pair) {
         return pair[0];
       });
